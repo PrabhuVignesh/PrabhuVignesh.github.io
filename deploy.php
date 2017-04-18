43a73f3c4e6bd4ef028ad8f46ba42d27e08d8203
PrabhuVignesh <prabhu.vignesh1990@gmail.com>
2017-04-18 15:05:47 +0530

    change in deploy

--- deploy.php
+++ deploy.php
@@ -31,9 +31,10 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
+	<title>Prabhu Profile auto deploy</title>
+	<link href="img/profile1.jpg" rel="icon" type="image/jpg">
 </head>
-<body style="background-color: #2196F3; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<body style="background-color: #425265; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
 <pre>
  .  ____  .    ____________________________
  |/      \|   |                            |
