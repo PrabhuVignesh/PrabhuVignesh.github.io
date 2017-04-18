aafff10a441023b9ce21612b7e5fd1299067ecac
PrabhuVignesh <prabhu.vignesh1990@gmail.com>
2017-04-18 17:15:56 +0530

    Adding submit form in static page

--- mail_handler.php
+++ mail_handler.php
@@ -1,10 +1,10 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "email@example.com"; // this is your Email address
+    $to = "prabhu.vignesh1990@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
-    $subject = "Form submission";
+    $subject = $_POST['first_name'] ."From Form submission";
     $subject2 = "Copy of your form submission";
     $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
     $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
